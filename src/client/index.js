241ce8ad669c15ba2702ace2e97c7b013610f3aa
nichoth <nichoth@gmail.com>
2019-12-04 22:07:31 -0800

    add deps

---
+++ src/client/index.js
@@ -0,0 +1,41 @@
+var ssbClient = require('ssb-client')
+var ssbKeys = require('ssb-keys')
+var manifest = {"auth":"async","address":"sync","manifest":"sync","multiserver":{"parse":"sync","address":"sync"},"multiserverNet":{},"get":"async","createFeedStream":"source","createLogStream":"source","messagesByType":"source","createHistoryStream":"source","createUserStream":"source","createWriteStream":"sink","links":"source","add":"async","publish":"async","getAddress":"sync","getLatest":"async","latest":"source","latestSequence":"async","whoami":"sync","progress":"sync","status":"sync","getVectorClock":"async","version":"sync","help":"sync","seq":"async","usage":"sync","clock":"async","gossip":{"add":"sync","remove":"sync","connect":"async","disconnect":"async","changes":"source","reconnect":"sync","disable":"sync","enable":"sync","ping":"duplex","get":"sync","peers":"sync","help":"sync"},"replicate":{"changes":"source","upto":"source","request":"sync","block":"sync"},"backlinks":{"read":"source"}}
+
+// simplest usage, connect to localhost sbot
+// this will cb with an error if an sbot server is not running
+// ssbClient(function (err, sbot) {
+// ...
+// })
+
+function start (cb) {
+    var keys = ssbKeys.loadOrCreateSync('./app-private.key')
+    ssbClient(
+        keys,                // optional, defaults to ~/.ssb/secret
+        {
+            host: 'localhost', // optional, defaults to localhost
+            port: 8008,        // optional, defaults to 8008
+            key: keys.id,      // optional, defaults to keys.id
+
+            caps: {
+                // random string for `appKey` in secret-handshake
+                shs: '123'
+            },
+
+            // optional, muxrpc manifest. Defaults to ~/.ssb/manifest.json
+            manifest: manifest
+
+        },
+        function (err, sbot, config) {
+            cb(err, sbot, config)
+            console.log('cb', err, sbot)
+        }
+    )
+}
+
+if (require.main === module) {
+    start()
+}
+
+module.exports = start
+
