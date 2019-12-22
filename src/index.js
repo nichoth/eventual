850b6e98bbbbd2646f8a740d3b66147c7292fb3f
nichoth <nichoth@gmail.com>
2019-12-22 13:48:31 -0800

    move things around

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 var ok = require('@nichoth/ok')
 var struct = require('observ-struct')
 var observ = require('observ')
+var Client = require('./client')
+var View = require('./view')
+var subscribe = require('./subscribe')
+
 var state = struct({
     foo: observ('world'),
     route: struct({})  // required
 })
-var Client = require('./client')
-var View = require('./view')
-var subscribe = require('./subscribe')
 
 Client({}, function (err, sbot) {
     sbot.whoami(function (err, who) {
