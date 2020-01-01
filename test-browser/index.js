7198224716478ac6e7d160f16989ce13aa44cc77
nichoth <nichoth@gmail.com>
2019-12-31 20:03:38 -0800

    test hack works

--- test-browser/index.js
+++ test-browser/index.js
@@ -1,14 +1,20 @@
 var test = require('tape')
 var App = require('../src')
 
+var _sbot
 test('doesnt explode', function (t) {
     App(function (err, sbot) {
         t.error(err)
         t.ok(sbot)
-        sbot.close(function (err) {
-            t.error(err)
-            t.end()
-        })
+        _sbot = sbot
+        t.end()
+    })
+})
+
+test('all done', function (t) {
+    _sbot.close(function (err) {
+        t.error(err, 'no error')
+        t.end()
     })
 })
 
