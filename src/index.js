94fe682ed15a88a27f44421f49f0303275b2180e
nichoth <nichoth@gmail.com>
2020-01-09 14:57:05 -0800

    works i guess

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var subscribe = require('./subscribe')
 var State = require('./state')
 var View = require('./view')
 var evs = require('./EVENTS')
+var S = require('pull-stream')
 
 var state = State()
 var { view } = ok(state, View, document.getElementById('content'))
@@ -12,6 +13,24 @@ Client({}, function (err, sbot) {
     if (err) {
         throw err
     }
+    console.log('sbot', sbot)
+
+    S(
+        sbot.createFeedStream(),
+        S.through(console.log.bind(console)),
+        S.onEnd(function (err) {
+            console.log('done', err)
+        })
+    )
+
+    var msg = {
+        type: 'post',
+        test: 'first post'
+    }
+    // sbot.publish(msg, function (err, data) {
+    //     console.log('pub', err, data)
+    // })
+    
     // sbot.whoami(function (err, who) {
     //     console.log('who', err, who)
     // })
