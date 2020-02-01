a647872cef8dbedef8e6c2be0424e7897bc2c703
nichoth <nichoth@gmail.com>
2020-01-31 21:27:28 -0800

    broken

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,6 +1,8 @@
 var evs = require('./EVENTS')
 var xtend = require('xtend')
-// var S = require('pull-stream')
+var S = require('pull-stream')
+// var blobFiles = require('ssb-blob-files')
+var fileReaderStream = require('filereader-pull-stream')
 
 function subscribe({ state, view, sbot }) {
     view.on(evs.hello.world, () => state.foo.set('bar'))
@@ -19,9 +21,32 @@ function subscribe({ state, view, sbot }) {
     })
 
     view.on(evs.profile.setAvatar, function (ev) {
+        var reader = new FileReader()
+        reader.onload = function (ev) {
+            console.log('ev', ev)
+            // console.log('.result', ev.target.result)
+        }
+        reader.readAsDataURL(ev.target.files[0])
+
+        // take reader.result and turn it into a pull stream
+        // see ssb-blob-files for an example
+        // then pipe it to sbot.blobs.add
+        // check that you get back a hash (file id) from that method call
+
         console.log('setAvatar', ev.target.files)
         console.log('setAvatar', ev.target.value)
-   })
+        S(
+            fileReaderStream(ev.target.files[0]),
+            sbot.blobs.add(function (err, hash) {
+                console.log('please work', err, hash)
+            })
+        )
+
+        // blobFiles(ev.target.files, sbot, function (err, res) {
+        //     console.log('here', err, res)
+        // })
+    })
 }
 
 module.exports = subscribe
+
