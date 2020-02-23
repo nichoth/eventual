3b652122e17f40c1c136f8c9678a62f06152404b
nichoth <nichoth@gmail.com>
2020-02-23 14:00:44 -0800

    dom stuff

--- src/view/pubs.js
+++ src/view/pubs.js
@@ -1,8 +1,27 @@
-var { h } = require('preact')
+var { h, Component } = require('preact')
 var evs = require('../EVENTS')
 
-function Pubs (props) {
-    return <div>Pubs</div>
+class Pubs extends Component {
+    constructor (props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+
+    submit (ev) {
+        ev.preventDefault()
+        console.log('submit', ev)
+        console.log('invite', ev.target.elements.invite.value)
+    }
+
+    render (props) {
+        return <div>
+            Add a pub
+            <form onSubmit={this.submit}>
+                <input type="text" id="invite" name="invite" />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    }
 }
 
 module.exports = Pubs
